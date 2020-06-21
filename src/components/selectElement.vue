7c5b5005bbeccb6b143384ded4fd32ed663daed5
singleDog <1054932242@qq.com>
2020-06-21 15:54:26 +0800

    Update selectElement.vue

--- src/components/selectElement.vue
+++ src/components/selectElement.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="select-element-wrap">
     <div class="select-element-main">
-      <h2 class="select-element-title">为将要附魔的武器选择合适的技能组合</h2>
+      <h1 class="select-element-title">为将要附魔的武器选择合适的技能组合</h1>
       <a-transfer
         class="select-skills-transfer"
         :data-source="skillData"
@@ -15,26 +15,28 @@
         @scroll="handleScroll"
       />
       <div class="element-results">
-        <h3>所需附魔元素</h3>
-        <a-tag color="#2db7f5" v-for="element in selectedElement" :key="element">{{ element }}</a-tag>
+        <h1 class="element-results-title">所需附魔元素</h1>
+        <a-tag color="#2db7f5" v-for="element in selectedElement"
+          :key="element">{{ element }}</a-tag>
       </div>
       <a-alert message="所选元素超出4槽位上限" type="warning" show-icon v-if="selectedElement.length>4"/>
     </div>
   </div>
 </template>
 <script>
-import { options, flags, masks } from "../data.js";
+import { options, flags, masks } from '../data';
+
 const skills = Object.keys(masks);
 export default {
   data() {
     const skillData = [];
     const selectedElement = [];
-    for (let i = 0; i < skills.length; i++) {
+    for (let i = 0; i < skills.length; i += 1) {
       skillData.push({
         key: i.toString(),
         title: `${skills[i]}`,
         description: `${masks[skills[i]].description}`,
-        disabled: false
+        disabled: false,
       });
     }
 
@@ -45,30 +47,28 @@ export default {
       targetKeys: oriTargetKeys,
       selectedKeys: [],
       disabled: false,
-      selectedElement
+      selectedElement,
     };
   },
   methods: {
-    handleChange(nextTargetKeys, direction, moveKeys) {
+    handleChange(nextTargetKeys) {
       this.targetKeys = nextTargetKeys;
       this.updateElementGroupResult();
     },
     handleSelectChange(sourceSelectedKeys, targetSelectedKeys) {
       this.selectedKeys = [...sourceSelectedKeys, ...targetSelectedKeys];
     },
-    handleScroll(direction, e) {},
     handleDisable(disabled) {
       this.disabled = disabled;
     },
     updateElementGroupResult() {
-      let currentMask = this.targetKeys
+      const currentMask = this.targetKeys
         .map(i => masks[skills[i]].flag)
         .reduce((acc, cur) => acc | cur, 0);
       this.selectedElement = Object.keys(flags).filter(
-        i => (flags[i] & currentMask) !== 0
-      );
-    }
-  }
+        i => (flags[i] & currentMask) !== 0);
+    },
+  },
 };
 </script>
 <style>
@@ -80,6 +80,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius:40px;
   min-height: 100vh;
 }
 .select-element-main {
@@ -98,16 +99,14 @@ export default {
     rgba(255, 255, 255, 0.8) 0%,
     rgba(255, 255, 255, 0.5) 100%
   ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ffffff',GradientType=0 ); /* IE6-9 */
   padding: 20px;
-  max-height: 80vh;
+  height: 80vh;
   max-width: 80vw;
   min-width: 80vw;
   overflow: auto;
 }
 .select-element-title {
   text-align: center;
-  color: white;
 }
 .select-skills-transfer {
   display: flex;
